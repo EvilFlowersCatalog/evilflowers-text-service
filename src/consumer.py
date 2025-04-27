e38cbd9457c599c098ea2d2eed10610e88c58ae6
Patrik Kozlik <patrik.kozlik@gmail.com>
2025-04-27 12:22:36 +0200

    feat: added elastic search to text service

--- src/consumer.py
+++ src/consumer.py
@@ -3,6 +3,8 @@ from config.consumer_config import consumer_conf, consumer_subscriptions
 from text_handler.TextService import TextHandler
 import json
 
+from elvira_elasticsearch_client import ElasticsearchClient 
+
 class MyConsumer(DefaultConsumer):
     def msg_process(self, msg):
         
@@ -12,7 +14,12 @@ class MyConsumer(DefaultConsumer):
         text_handler = TextHandler(json_object["document_path"])
         text = text_handler.extract_text(json_object["found_toc"])
         
+        client = ElasticsearchClient()
         print(text)
+        client.save_extracted_text_to_elasticsearch(document_id=json_object["document_id"],
+                                                    text_data=text)
+        
+        
 
 if __name__ == "__main__":
     consumer = MyConsumer(consumer_conf, consumer_subscriptions)
